578572b976b994d86bed639b9d9f2d81caf85dc2
Jose Mosquera <jose-14mosquera@hotmail.com>
2024-07-10 19:53:46 -0500

    Agregar archivos de proyecto.

---
+++ ejercicio2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+namespace CalculadoraBasica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Pedir el primer número al usuario
+            Console.Write("Ingrese el primer número: ");
+            double numero1 = double.Parse(Console.ReadLine());
+
+            // Pedir el segundo número al usuario
+            Console.Write("Ingrese el segundo número: ");
+            double numero2 = double.Parse(Console.ReadLine());
+
+            // Realizar las operaciones básicas
+            Console.WriteLine("\nSuma: {0} + {1} = {2}", numero1, numero2, numero1 + numero2);
+            Console.WriteLine("Resta: {0} - {1} = {2}", numero1, numero2, numero1 - numero2);
+            Console.WriteLine("Multiplicación: {0} * {1} = {2}", numero1, numero2, numero1 * numero2);
+            Console.WriteLine("División: {0} / {1} = {2}", numero1, numero2, numero1 / numero2);
+            Console.WriteLine("Modulo: {0} % {1} = {2}", numero1,numero2, numero1 % numero2);
+            Console.WriteLine($"{numero1}<{numero2} es {numero1 < numero2}");
+
+            // Pausar la ejecución por 2 segundos
+            Thread.Sleep(2000); // Pausa por 2 segundos (2000 milliseconds)
+
+            // Mensaje para indicar que se puede continuar
+            Console.WriteLine("\nPresione cualquier tecla para continuar...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ intento1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace intento1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese su nombre: ");
+                string NombreA = Console.ReadLine();
+            Console.Write("ingrese su edad: ");
+            int EdadA = Console.Read();
+            Console.WriteLine("ingrese una letra");
+            char letra1 = char.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese un valor decimal");
+            double dec_1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese un numero");
+            int numeroal = int.Parse(Console.ReadLine());
+            int numero77 = 85;
+            int numero12 = 17;
+            int auxiliar = 0;
+            auxiliar = numero77;
+            numero12 = auxiliar;
+            Console.WriteLine(auxiliar);
+            Console.ReadKey();
+            
+            
+        }
+    }
+}
