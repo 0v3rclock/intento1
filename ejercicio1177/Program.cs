9f32fca8622183f2cac09062edcaa19cad1a7479
Jose Mosquera <jose-14mosquera@hotmail.com>
2024-07-17 19:25:59 -0500

    crecion ejercicio1177

---
+++ ejercicio1177/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ejercicio1177
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un numero a imprimir: ");
+            int num = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= num; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Thread.Sleep(1000);
+            Console.WriteLine("Pulse una tecla para finalizar...");
+            Console.ReadKey();
+        }
+    }
+}
