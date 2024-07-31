a3c73ca7447efc9af7ce8dd8f4635ba47d9dd440
Jose Mosquera <jose-14mosquera@hotmail.com>
2024-07-31 18:45:11 -0500

    clase227 añadida

--- clase227/Program.cs
+++ clase227/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -15,22 +16,47 @@ namespace clase227
             int num = int.Parse(Console.ReadLine());
             Console.WriteLine("escriba un numero de limite ascendente: ");
             int lnuma = int.Parse(Console.ReadLine());
+            bool esPar = num % 2 == 0;
+            int ContPar = 0;
+            int ContImpar = 0;
             Console.WriteLine("\norden asccendente");
             while (num <=lnuma)
             {
-                Console.WriteLine("hola" + num++);
+                Console.WriteLine(num++);
             }
+            if (esPar)
+            {
+                ContPar++;
+            }
+            else
+            {
+                ContImpar++;
+            }
+            Console.WriteLine($"\n los numeros pares son {ContPar}, mientras que los impares son {ContImpar}\n");
+
             Thread.Sleep(2000);
             Console.WriteLine("ingrese un numero alto para base descendente");
             int num2 = int.Parse(Console.ReadLine());
             Console.WriteLine("ingrese un numero de limite descendente: ");
             int lnumd= int.Parse(Console.ReadLine());
+            bool esPar2= num % 2 == 0;
+            int Contpar2 = 0;
+            int ContImpar2 = 0;
             Console.WriteLine("\norden descendente");
             while (num2 >= lnumd)
             {
-                Console.WriteLine("hola" + num2--);
+                Console.WriteLine(num2--);
             }
-                    Thread.Sleep(5000);//pausa el programa por 5 segundos
+            if (esPar2)
+            {
+                Contpar2++;
+            }
+            else
+            {
+                ContImpar2++;
+            }
+            Console.WriteLine($"\n los numeros pares son {Contpar2}, mientras que los impares son {ContImpar2}\n");
+            Thread.Sleep(5000);//pausa el programa por 5 segundos
             
 
         }
---
+++ while/Program.cs
@@ -0,0 +1,25 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace @while
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0;
+            int num2 = 10;
+            do
+            {
+                Console.WriteLine(num2--);
+                
+            }
+            while (num2 >= num1);
+            Thread.Sleep(2500);
+        }
+    }
+}
